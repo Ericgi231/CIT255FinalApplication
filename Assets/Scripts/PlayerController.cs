b56d51af22d0a74cd31d56e53bd4042e89316b91
Eric Grant <ericgi231@gmail.com>
2018-11-27 23:19:22 -0500

    altered movement, tested prefabs

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,47 +4,63 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-    public float moveSpeed;
-    public float rotationSpeed;
+    public float characterMoveSpeed;
+    public float cameraRotationSpeed;
     public float verticalRotationRange;
 
-    public float verticalRotation;
-    public float horizontalRotation;
+    private float verticalRotation;
+    private float horizontalRotation;
 
-    public CharacterController cc;
+    private float xSpeed;
+    private float ySpeed;
+    private float zSpeed;
+
+    private float yVelocity = 0;
+
+    private CharacterController cc;
 
     // Use this for initialization
-    void Start () {
+    void Start() {
         Cursor.lockState = CursorLockMode.Locked;
 
-        moveSpeed = 10.0f;
-        rotationSpeed = 2.0f;
+        characterMoveSpeed = 10.0f;
+        cameraRotationSpeed = 2.0f;
         verticalRotationRange = 90.0f;
 
         cc = GetComponent<CharacterController>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    }
 
-        //rotate
+    // Update is called once per frame
+    void Update() {
+        UpdateCameraDirection();
+        UpdatePlayerPosition();
+    }
+
+    private void UpdateCameraDirection()
+    {
         //horizontal
-        horizontalRotation = Input.GetAxis("Mouse X") * rotationSpeed;
+        horizontalRotation = Input.GetAxis("Mouse X") * cameraRotationSpeed;
         transform.Rotate(0, horizontalRotation, 0);
 
         //vertical
-        verticalRotation -= Input.GetAxis("Mouse Y") * rotationSpeed;
+        verticalRotation -= Input.GetAxis("Mouse Y") * cameraRotationSpeed;
         verticalRotation = Mathf.Clamp(verticalRotation, -verticalRotationRange, verticalRotationRange);
         Camera.main.transform.localRotation = Quaternion.Euler(verticalRotation, 0, 0);
+    }
 
-        //move
-        float verticalSpeed = Input.GetAxis("Vertical") * moveSpeed;
-        float horizontalSpeed = Input.GetAxis("Horizontal") * moveSpeed;
+    private void UpdatePlayerPosition()
+    {
+        yVelocity += Physics.gravity.y * Time.deltaTime;
+        zSpeed = Input.GetAxis("Vertical") * characterMoveSpeed;
+        xSpeed = Input.GetAxis("Horizontal") * characterMoveSpeed;
+        if (Input.GetButton("Jump") && cc.isGrounded)
+        {
+            yVelocity = 4;
+        }
 
-        Vector3 momentum = new Vector3(horizontalSpeed,0,verticalSpeed);
-
+        Vector3 momentum = new Vector3(xSpeed, yVelocity, zSpeed);
         momentum = transform.rotation * momentum;
 
-        cc.SimpleMove(momentum);
-	}
+        cc.Move(momentum * Time.deltaTime);
+    }
 }
