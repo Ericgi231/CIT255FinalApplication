35fa8c2cd5a192698870415588568df4f9e3e24a
Eric Grant <ericgi231@gmail.com>
2018-12-01 22:58:24 -0500

    soil prefab started

--- Assets/Scripts/SoilLogic.cs
+++ Assets/Scripts/SoilLogic.cs
@@ -4,9 +4,20 @@ using UnityEngine;
 
 public class SoilLogic : MonoBehaviour {
 
+    Material mat;
+    SoilState state;
+
+    public enum SoilState
+    {
+        RAW,
+        TILLED,
+        WATTERED
+    }
+    
+
 	// Use this for initialization
 	void Start () {
-		
+        mat = GetComponent<MeshRenderer>().material;
 	}
 	
 	// Update is called once per frame
@@ -16,6 +27,25 @@ public class SoilLogic : MonoBehaviour {
 
     void OnMouseDown()
     {
-          
+        state = SoilState.WATTERED;
+        SetState();
+    }
+
+    void SetState()
+    {
+        switch (state)
+        {
+            case SoilState.RAW:
+                mat.color = new Color32(120,30,0,0);
+                break;
+            case SoilState.TILLED:
+                mat.color = new Color32(150, 60, 0, 0);
+                break;
+            case SoilState.WATTERED:
+                mat.color = new Color32(150, 60, 50, 0);
+                break;
+            default:
+                break;
+        }
     }
 }
